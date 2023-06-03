fae2620898bcf8cfbe9b855b290532903a9eb3d0
KukuruzaAndrey <andreyku6@gmail.com>
2023-06-03 22:56:46 +0300

    cleanup and reformat

--- src/screens.h
+++ src/screens.h
@@ -6,21 +6,6 @@
 *
 *   Copyright (c) 2014-2022 Ramon Santamaria (@raysan5)
 *
-*   This software is provided "as-is", without any express or implied warranty. In no event
-*   will the authors be held liable for any damages arising from the use of this software.
-*
-*   Permission is granted to anyone to use this software for any purpose, including commercial
-*   applications, and to alter it and redistribute it freely, subject to the following restrictions:
-*
-*     1. The origin of this software must not be misrepresented; you must not claim that you
-*     wrote the original software. If you use this software in a product, an acknowledgment
-*     in the product documentation would be appreciated but is not required.
-*
-*     2. Altered source versions must be plainly marked as such, and must not be misrepresented
-*     as being the original software.
-*
-*     3. This notice may not be removed or altered from any source distribution.
-*
 **********************************************************************************************/
 
 #ifndef SCREENS_H
@@ -38,10 +23,6 @@ extern GameScreen currentScreen;
 extern Font font;
 extern Music music;
 extern Sound fxCoin;
-
-#ifdef __cplusplus
-extern "C" {            // Prevents name mangling of functions
-#endif
 
 //----------------------------------------------------------------------------------
 // Logo Screen Functions Declaration
@@ -88,8 +69,5 @@ void DrawEndingScreen(void);
 void UnloadEndingScreen(void);
 int FinishEndingScreen(void);
 
-#ifdef __cplusplus
-}
-#endif
-
 #endif // SCREENS_H
+
