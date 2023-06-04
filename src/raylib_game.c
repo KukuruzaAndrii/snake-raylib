97f0fcb62ce83f98e71da20c6d98df77853e0b6b
Andrii Kukuruza <kukuruza.a@ajax.systems>
2023-06-04 17:23:18 +0300

    refactor

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -251,57 +251,51 @@ static void DrawTransition(void) {
 
 // Update game frame
 static void UpdateFrame(void) {
-	if (!onTransition) {
-		switch(currentScreen) {
-		case LOGO: {
-			UpdateLogoScreen();
-
-			if (FinishLogoScreen()) {
-				TransitionToScreen(TITLE);
-			}
-
-		} break;
-		case TITLE: {
-			UpdateTitleScreen();
-
-			if (FinishTitleScreen() == 1) {
-				TransitionToScreen(OPTIONS);
-			} else if (FinishTitleScreen() == 2) {
-				TransitionToScreen(GAMEPLAY);
-			}
-
-		} break;
-		case OPTIONS:{
-			UpdateOptionsScreen();
-
-			if (FinishOptionsScreen()) {
-				TransitionToScreen(TITLE);
-			}
-
-		} break;
-		case GAMEPLAY:{
-			UpdateGameplayScreen();
-
-			if (FinishGameplayScreen() == 1) {
-				TransitionToScreen(ENDING);
-			}
-			//else if (FinishGameplayScreen() == 2) TransitionToScreen(TITLE);
-
-		} break;
-		case ENDING: {
-			UpdateEndingScreen();
-
-			if (FinishEndingScreen() == 1) {
-				TransitionToScreen(TITLE);
-			}
-
-		} break;
-		default: break;
-		}
-	} else {
+	if (onTransition) {
 		UpdateTransition();	// Update transition (fade-in, fade-out)
+		return;
 	}
+	switch(currentScreen) {
+	case LOGO: {
+		UpdateLogoScreen();
 
+		if (FinishLogoScreen()) {
+			TransitionToScreen(TITLE);
+		}
+	} break;
+	case TITLE: {
+		UpdateTitleScreen();
+
+		if (FinishTitleScreen() == 1) {
+			TransitionToScreen(OPTIONS);
+		} else if (FinishTitleScreen() == 2) {
+			TransitionToScreen(GAMEPLAY);
+		}
+	} break;
+	case OPTIONS:{
+		UpdateOptionsScreen();
+
+		if (FinishOptionsScreen()) {
+			TransitionToScreen(TITLE);
+		}
+	} break;
+	case GAMEPLAY:{
+		UpdateGameplayScreen();
+
+		if (FinishGameplayScreen() == 1) {
+			TransitionToScreen(ENDING);
+		}
+		//else if (FinishGameplayScreen() == 2) TransitionToScreen(TITLE);
+	} break;
+	case ENDING: {
+		UpdateEndingScreen();
+
+		if (FinishEndingScreen() == 1) {
+			TransitionToScreen(TITLE);
+		}
+	} break;
+	default: break;
+	}
 }
 
 static void DrawFrame(void) {
