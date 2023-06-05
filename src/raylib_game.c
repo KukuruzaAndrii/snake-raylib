c64c9718e991157d23de27e34a5a0865a222fdb1
KukuruzaAndrey <andreyku6@gmail.com>
2023-06-05 23:14:47 +0300

    move;eat

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -18,7 +18,10 @@
 #include "raylib.h"
 #include "screen_logo.h"
 
-#define GRID_PX 20
+// GRID_SIZE
+#define GRID_PX 43
+#define EAT_PX_SZ (GRID_PX/2)
+
 #define GRID_W_COUNT (screenWidth/GRID_PX + 1)
 #define GRID_H_COUNT (screenHeight/GRID_PX + 1)
 //----------------------------------------------------------------------------------
@@ -35,28 +38,18 @@ Sound fxCoin = { 0 };
 static const int screenWidth = 1920;
 static const int screenHeight = 1080;
 
-//----------------------------------------------------------------------------------
-// Local Functions Declaration
-//----------------------------------------------------------------------------------
-static void init();
-static void deinit();
-
-static void UpdateFrame(void);	    // Update one frame
-static void DrawFrame(void);	    // Draw one frame
-
 static struct node * addNode(struct node *head, int x, int y);
 
-#define foreach_node(_n) for(struct node *_n = Sn; _n != NULL; _n = _n->next)
+#define foreach_node(_head, _n) for(struct node *_n = _head; _n != NULL; _n = _n->next)
 
 struct node {
 	int x;
 	int y;
 	struct node *next;
 };
-struct node *Sn = NULL;
 
 static int framesCounter = 0;
-static int tickInFrames = 5;
+static int tickInFrames = 8;
 static void tick();
 static bool isTicked = false;
 
@@ -67,43 +60,20 @@ enum dir {
 	DIR_LEFT,
 };
 
-enum dir cur_dir = DIR_RIGHT;
-//----------------------------------------------------------------------------------
-// Main entry point
-//----------------------------------------------------------------------------------
-int main(void) {
-	init();
+#define IS_DIR_HOR(_dir) ((_dir) == DIR_LEFT || (_dir) == DIR_RIGHT)
+#define IS_DIR_VER(_dir) ((_dir) == DIR_UP || (_dir) == DIR_DOWN)
 
-	SetTargetFPS(60);	    // Set our game to run at 60 frames-per-second
-	// TraceLog(LOG_WARNING, "%d %d", GRID_W_COUNT, GRID_H_COUNT);
+struct game_ctx {
+	struct node * head;
+	enum dir dir;
+	Vector2 eat;
+};
+
+struct game_ctx * G = NULL;
 
 
-	struct node head = {.x = 10, .y = 10};
-	addNode(addNode(&head, 9, 10), 8, 10);
-	Sn = &head;
-	// Main game loop
-	while (!WindowShouldClose()) {   // Detect window close button or ESC key
-		//	UpdateMusicStream(music);	    // NOTE: Music keeps playing between screens
-		tick();
-		UpdateFrame();
-		DrawFrame();
-	}
 
-	deinit();
 
-	return 0;
-}
-
-static void tick() {
-	framesCounter += 1;
-	if (isTicked) {
-		isTicked = false;
-	}
-	if (framesCounter >= tickInFrames) {
-		framesCounter = 0;
-		isTicked = true;
-	}
-}
 static void init() {
 	InitWindow(screenWidth, screenHeight, "raylib game template");
 	ToggleFullscreen();
@@ -121,6 +91,152 @@ static void init() {
 	InitLogoScreen();
 }
 
+
+
+struct game_ctx* init_game(void) {
+	struct game_ctx *ctx = (struct game_ctx *)malloc(sizeof(struct game_ctx)) ;
+
+	struct node *n = (struct node *)malloc(sizeof(struct node));
+	n->x = 10;
+	n->y = 10;
+	n->next = NULL;
+
+	addNode(addNode(n, 9, 10), 8, 10);
+
+	ctx->head = n;
+	ctx->dir = DIR_RIGHT;
+
+	ctx->eat = (struct Vector2){.x = 20, .y = 15};
+
+	return ctx;
+}
+
+void handleControl(struct game_ctx *ctx) {
+	if (IsKeyPressed(KEY_UP)) {
+		if (IS_DIR_HOR(ctx->dir)) {
+			ctx->dir = DIR_UP;
+		}
+	} else if (IsKeyPressed(KEY_RIGHT)) {
+		if (IS_DIR_VER(ctx->dir)) {
+			ctx->dir = DIR_RIGHT;
+		}
+	} else if (IsKeyPressed(KEY_DOWN)) {
+		if (IS_DIR_HOR(ctx->dir)) {
+			ctx->dir = DIR_DOWN;
+		}
+	} else if (IsKeyPressed(KEY_LEFT)) {
+		if (IS_DIR_VER(ctx->dir)) {
+			ctx->dir = DIR_LEFT;
+		}
+	}
+}
+
+
+static void tick() {
+	framesCounter += 1;
+	if (isTicked) {
+		isTicked = false;
+	}
+	if (framesCounter >= tickInFrames) {
+		framesCounter = 0;
+		isTicked = true;
+	}
+}
+
+static struct node * addNode(struct node *head, int x, int y) {
+	struct node *n = (struct node *)malloc(sizeof(struct node));
+	n->x = x;
+	n->y = y;
+	n->next = NULL;
+	head->next = n;
+	return n;
+}
+
+static struct node* growSnake(struct game_ctx *g) {
+	struct node *n = (struct node *)malloc(sizeof(struct node));
+	switch (g->dir) {
+	case DIR_UP:
+		n->x = g->head->x;
+		n->y = g->head->y - 1;
+		break;
+	case DIR_RIGHT:
+		n->x = g->head->x + 1;
+		n->y = g->head->y;
+		break;
+	case DIR_DOWN:
+		n->x = g->head->x;
+		n->y = g->head->y + 1;
+		break;
+	case DIR_LEFT:
+		n->x = g->head->x - 1;
+		n->y = g->head->y;
+		break;
+	}
+	n->next = g->head;
+	return n;
+}
+
+static void shrinkSnake(struct game_ctx *g) {
+	foreach_node(g->head, n) {
+		if (n->next && n->next->next == NULL) {
+			free(n->next);
+			n->next = NULL;
+			break;
+		}
+	}
+}
+
+static void updateSnake(struct game_ctx *g) {
+	g->head = growSnake(g);
+	shrinkSnake(g);
+}
+
+static void updateEat(struct game_ctx *g) {
+
+}
+
+
+static void UpdateFrame(struct game_ctx *g) {
+	if (isTicked) {
+		updateSnake(g);
+		updateEat(g);
+	}
+}
+
+static void drawGrid(void) {
+	for (unsigned i = 0; i < GRID_W_COUNT; i++) {
+		DrawLine(i * GRID_PX, 0, i * GRID_PX, screenHeight, LIGHTGRAY);
+	}
+	for (unsigned i = 0; i < GRID_H_COUNT; i++) {
+		DrawLine(0, i * GRID_PX, screenWidth, i * GRID_PX, LIGHTGRAY);
+	}
+}
+
+static void drawSnake(struct game_ctx *g) {
+	foreach_node(g->head, n) {
+		// TraceLog(LOG_WARNING, "x=%d y=%d", n->x, n->y);
+		DrawRectangle(n->x * GRID_PX, n->y * GRID_PX, GRID_PX, GRID_PX, SKYBLUE);
+	}
+}
+
+static void drawEat(struct game_ctx *g) {
+	DrawRectangle(g->eat.x * GRID_PX + EAT_PX_SZ/2, g->eat.y * GRID_PX + EAT_PX_SZ/2, EAT_PX_SZ, EAT_PX_SZ, MAGENTA);
+}
+
+
+static void DrawFrame(struct game_ctx *g) {
+	BeginDrawing();
+
+	ClearBackground(RAYWHITE);
+	//DrawLogoScreen();
+	//DrawFPS(10, 10);
+	drawGrid();
+	drawSnake(g);
+	drawEat(g);
+
+	EndDrawing();
+}
+
 static void deinit() {
 	// Unload current screen data before closing
 	UnloadLogoScreen();
@@ -135,82 +251,25 @@ static void deinit() {
 	CloseWindow();	    // Close window and OpenGL context
 }
 
-static struct node * addNode(struct node *head, int x, int y) {
-	struct node *n = (struct node *)malloc(sizeof(struct node));
-	n->x = x;
-	n->y = y;
-	n->next = NULL;
-	head->next = n;
-	return n;
-}
+int main(void) {
+	init();
 
-static void updateSnake(void) {
-	struct node *n = (struct node *)malloc(sizeof(struct node));
-	switch (cur_dir) {
-	case DIR_UP:
-		n->x = Sn->x;
-		n->y = Sn->y - 1;
-		break;
-	case DIR_RIGHT:
-		n->x = Sn->x + 1;
-		n->y = Sn->y;
-		break;
-	case DIR_DOWN:
-		n->x = Sn->x;
-		n->y = Sn->y + 1;
-		break;
-	case DIR_LEFT:
-		n->x = Sn->x - 1;
-		n->y = Sn->y;
-		break;
-	}
-	n->next = Sn;
-	Sn = n;
-}
+	SetTargetFPS(60);
+	// TraceLog(LOG_WARNING, "%d %d", GRID_W_COUNT, GRID_H_COUNT);
 
-// Update game frame
-static void UpdateFrame(void) {
-	//UpdateLogoScreen();
-	if (IsKeyPressed(KEY_UP)) {
-		cur_dir = DIR_UP;
-	} else if (IsKeyPressed(KEY_RIGHT)) {
-		cur_dir = DIR_RIGHT;
-	} else if (IsKeyPressed(KEY_DOWN)) {
-		cur_dir = DIR_DOWN;
-	} else if (IsKeyPressed(KEY_LEFT)) {
-		cur_dir = DIR_LEFT;
+	G = init_game();
+
+	// Main game loop
+	while (!WindowShouldClose()) {
+		//	UpdateMusicStream(music);	    // NOTE: Music keeps playing between screens
+		tick();
+		handleControl(G);
+		UpdateFrame(G);
+		DrawFrame(G);
 	}
 
-	if (isTicked) {
-		updateSnake();
-	}
-	
+	deinit();
+
+	return 0;
 }
 
-static void drawGrid(void) {
-	for (unsigned i = 0; i < GRID_W_COUNT; i++) {
-		DrawLine(i * GRID_PX, 0, i * GRID_PX, screenHeight, BLACK);
-	}
-	for (unsigned i = 0; i < GRID_H_COUNT; i++) {
-		DrawLine(0, i * GRID_PX, screenWidth, i * GRID_PX, BLACK);
-	}
-}
-
-static void drawSnake(void) {
-	foreach_node(n) {
-		// TraceLog(LOG_WARNING, "x=%d y=%d", n->x, n->y);
-		DrawRectangle(n->x * GRID_PX, n->y * GRID_PX, GRID_PX, GRID_PX, BLUE);
-	}
-}
-
-static void DrawFrame(void) {
-	BeginDrawing();
-
-	ClearBackground(RAYWHITE);
-	//DrawLogoScreen();
-	//DrawFPS(10, 10);
-	drawGrid();
-	drawSnake();
-
-	EndDrawing();
-}
