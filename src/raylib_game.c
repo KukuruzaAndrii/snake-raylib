37b50381c601b9a4ff1ef6cdb631a7afe3a1ecd7
KukuruzaAndrey <andreyku6@gmail.com>
2023-06-06 23:36:41 +0300

    score;game over

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -22,8 +22,11 @@
 #define GRID_PX 43
 #define EAT_PX_SZ (GRID_PX/2)
 
-#define GRID_W_COUNT (screenWidth/GRID_PX + 1)
-#define GRID_H_COUNT (screenHeight/GRID_PX + 1)
+#define SCREEN_WIDTH  1920
+#define SCREEN_HEIGHT 1080
+
+#define GRID_W_COUNT (SCREEN_WIDTH/GRID_PX + 1)
+#define GRID_H_COUNT (SCREEN_HEIGHT/GRID_PX + 1)
 //----------------------------------------------------------------------------------
 // Shared Variables Definition (global)
 // NOTE: Those variables are shared between modules through screens.h
@@ -35,8 +38,7 @@ Sound fxCoin = { 0 };
 //----------------------------------------------------------------------------------
 // Local Variables Definition (local to this module)
 //----------------------------------------------------------------------------------
-static const int screenWidth = 1920;
-static const int screenHeight = 1080;
+
 
 static struct node * addNode(struct node *head, int x, int y);
 
@@ -50,8 +52,6 @@ struct node {
 
 static int framesCounter = 0;
 static int tickInFrames = 8;
-static void tick();
-static bool isTicked = false;
 
 enum dir {
 	DIR_UP = 0,
@@ -72,6 +72,10 @@ struct game_ctx {
 	struct node * head;
 	enum dir dir;
 	struct eat eat;
+	int score;
+	unsigned is_ticked:1;
+	unsigned is_game_over:1;
+
 };
 
 struct game_ctx * G = NULL;
@@ -80,8 +84,8 @@ struct game_ctx * G = NULL;
 
 
 static void init() {
-	InitWindow(screenWidth, screenHeight, "raylib game template");
-//	ToggleFullscreen();
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib game template");
+	ToggleFullscreen();
 	InitAudioDevice();	    // Initialize audio device
 
 	// Load global data (assets that must be available in all screens, i.e. font)
@@ -99,7 +103,7 @@ static void init() {
 
 
 struct game_ctx* init_game(void) {
-	struct game_ctx *ctx = (struct game_ctx *)malloc(sizeof(struct game_ctx)) ;
+	struct game_ctx *g = (struct game_g *)malloc(sizeof(struct game_ctx)) ;
 
 	struct node *n = (struct node *)malloc(sizeof(struct node));
 	n->x = 10;
@@ -108,12 +112,15 @@ struct game_ctx* init_game(void) {
 
 	addNode(addNode(n, 9, 10), 8, 10);
 
-	ctx->head = n;
-	ctx->dir = DIR_RIGHT;
+	g->head = n;
+	g->dir = DIR_RIGHT;
 
-	ctx->eat = (struct eat){.x = 20, .y = 15};
+	g->eat = (struct eat){.x = 20, .y = 15};
+	g->score = 0;
+	g->is_ticked = 0;
+	g->is_game_over = 0;
 
-	return ctx;
+	return g;
 }
 
 void handleControl(struct game_ctx *ctx) {
@@ -137,14 +144,14 @@ void handleControl(struct game_ctx *ctx) {
 }
 
 
-static void tick() {
+static void tick(struct game_ctx *ctx) {
 	framesCounter += 1;
-	if (isTicked) {
-		isTicked = false;
+	if (ctx->is_ticked) {
+		ctx->is_ticked = false;
 	}
 	if (framesCounter >= tickInFrames) {
 		framesCounter = 0;
-		isTicked = true;
+		ctx->is_ticked = true;
 	}
 }
 
@@ -197,35 +204,41 @@ static void updateSnake(struct game_ctx *g) {
 
 static unsigned checkEat(struct game_ctx *g) {
 	if (g->head->x == g->eat.x && g->head->y == g->eat.y) {
-		g->eat.x = GetRandomValue(0, GRID_W_COUNT);
-		g->eat.y = GetRandomValue(0, GRID_H_COUNT);
+		g->eat.x = GetRandomValue(0, GRID_W_COUNT - 1);
+		g->eat.y = GetRandomValue(0, GRID_H_COUNT - 1);
+		g->score += 1;
 		return 1;
 	}
 	return 0;
 }
 
-static void updateEat(struct game_ctx *g) {
+static unsigned check_game_over(struct game_ctx *g) {
+	if (g->head->x == GRID_W_COUNT) {
+		return 1;
+	}
 
+	return 0;
 }
 
-
 static void UpdateFrame(struct game_ctx *g) {
-	if (isTicked) {
+	if (g->is_ticked) {
 		g->head = growSnake(g);
 
 		unsigned is_eat = checkEat(g);
 		if (!is_eat) {
 			shrinkSnake(g);
 		}
+
+		g->is_game_over = check_game_over(g);
 	}
 }
 
 static void drawGrid(void) {
 	for (int i = 0; i < GRID_W_COUNT; i++) {
-		DrawLine(i * GRID_PX, 0, i * GRID_PX, screenHeight, LIGHTGRAY);
+		DrawLine(i * GRID_PX, 0, i * GRID_PX, SCREEN_HEIGHT, LIGHTGRAY);
 	}
 	for (int i = 0; i < GRID_H_COUNT; i++) {
-		DrawLine(0, i * GRID_PX, screenWidth, i * GRID_PX, LIGHTGRAY);
+		DrawLine(0, i * GRID_PX, SCREEN_WIDTH, i * GRID_PX, LIGHTGRAY);
 	}
 }
 
@@ -240,6 +253,14 @@ static void drawEat(struct game_ctx *g) {
 	DrawRectangle(g->eat.x * GRID_PX + EAT_PX_SZ/2, g->eat.y * GRID_PX + EAT_PX_SZ/2, EAT_PX_SZ, EAT_PX_SZ, MAGENTA);
 }
 
+static void drawScore(struct game_ctx *g) {
+	DrawText("Score:", 40, 40, 40, DARKGRAY);
+	DrawText(TextFormat("%d", g->score), 190, 40, 40, RED);
+}
+
+static void drawGameOver(struct game_ctx *g) {
+	DrawText("GAME OVER", SCREEN_WIDTH/2 - 400, SCREEN_HEIGHT/2 - 70, 140, DARKGRAY);
+}
 
 static void DrawFrame(struct game_ctx *g) {
 	BeginDrawing();
@@ -250,6 +271,11 @@ static void DrawFrame(struct game_ctx *g) {
 	drawGrid();
 	drawSnake(g);
 	drawEat(g);
+	drawScore(g);
+	if (g->is_game_over) {
+		//TraceLog(LOG_WARNING, "is_game_over", GRID_W_COUNT, GRID_H_COUNT);
+		drawGameOver(g);
+	}
 
 	EndDrawing();
 }
@@ -279,7 +305,7 @@ int main(void) {
 	// Main game loop
 	while (!WindowShouldClose()) {
 		//	UpdateMusicStream(music);	    // NOTE: Music keeps playing between screens
-		tick();
+		tick(G);
 		handleControl(G);
 		UpdateFrame(G);
 		DrawFrame(G);
