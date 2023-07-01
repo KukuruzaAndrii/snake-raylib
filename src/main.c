6e903cd12d44c93901011e2a8c8489dfb8a565ca
KukuruzaAndrey <andreyku6@gmail.com>
2023-07-01 22:42:46 +0300

    cleanup

--- src/main.c
+++ src/main.c
@@ -1,16 +1,11 @@
-/*******************************************************************************************
- *
- *   raylib game template
- *
- *   <Game title>
- *   <Game description>
+/*
+ *   Snake Hike Game
+ *   It's game about snake mechanic in different views
  *
  *   This game has been created using raylib (www.raylib.com)
  *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
  *
- *   Copyright (c) 2021 Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
+ */
 
 #include <stddef.h>
 #include <stdlib.h>
